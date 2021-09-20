2701c4ea425e122aafaeac1cd30ec3c38a8e00cc
Espen Land Stokkeland <espels16@student.uia.no>
2021-09-20 15:33:11 +0200

    Feature

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,11 +1,14 @@
 import "./ExpenseItem.css";
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Scooter insurance";
+  const expenseAmount = 180.46;
   return (
     <div className="expense-item">
-      <div>29. juni 2021</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Scooter forsikring</h2>
-        <div className="expense-item__price">178 kroner</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expenseAmount}</div>
       </div>
     </div>
   );
