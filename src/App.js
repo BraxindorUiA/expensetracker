9ef0efd9887d63cca20ff40bbb1f8ac8dadae61d
Espen Land Stokkeland <espels16@student.uia.no>
2021-09-20 11:14:52 +0200

    Feature

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import ExpenseItem from "./Components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
+      <h1>There really should've been a h1 by default</h1>
     </div>
   );
 }
---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>29. juni 2021</div>
+      <div>
+        <h2>Scooter forsikring</h2>
+        <div>178 kroner</div>
+      </div>
+    </div>
+  );
+}
+export default ExpenseItem;
