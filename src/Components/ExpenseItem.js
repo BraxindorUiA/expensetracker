56d3637dc3a9d63a8d4f44da2faa21826c86642a
Espen Land Stokkeland <espels16@student.uia.no>
2021-09-20 13:54:19 +0200

    Feature

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,10 +1,11 @@
+import "./ExpenseItem.css";
 function ExpenseItem() {
   return (
-    <div>
+    <div className="expense-item">
       <div>29. juni 2021</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Scooter forsikring</h2>
-        <div>178 kroner</div>
+        <div className="expense-item__price">178 kroner</div>
       </div>
     </div>
   );
