f6e74ea4b3e4d8fc316b35549a506822ccb52d7d
Espen Land Stokkeland <espels16@student.uia.no>
2021-09-27 11:04:27 +0200

    Added props for reuseability

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,16 +1,15 @@
 import "./ExpenseItem.css";
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = "Scooter insurance";
-  const expenseAmount = 180.46;
+
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount} kroner</div>
       </div>
     </div>
   );
 }
+
 export default ExpenseItem;
